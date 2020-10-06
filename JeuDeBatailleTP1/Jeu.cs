5bc4602d5aed6da6b9e7807f185075a135b60d03
DaniilSemashko <dsembusiness@gmail.com>
2020-10-06 18:56:57 -0400

    Concept was flooded and created way too many unecessary complicated class to class relations

--- JeuDeBatailleTP1/Jeu.cs
+++ JeuDeBatailleTP1/Jeu.cs
@@ -4,7 +4,59 @@ using System.Text;
 
 namespace JeuDeBatailleTP1
 {
-    class Jeu
+    static class Jeu
     {
+        static public void DemarrageProg(Joueur j1, Joueur j2)
+        {
+            CreationPaquetsJoueurs.GenererPaquetJoueurs(j1, j2);
+            CreationPaquetsJoueurs.DefinirNom(j1);
+        }
+
+        static public void MelangerLesPaquetsDesJoueurs(Joueur j1, Joueur j2)
+        {
+            j1.paquetEnJeu.MelangerPaquet();
+            j2.paquetEnJeu.MelangerPaquet();
+        }
+
+        static public void PiocherUneCarte(Joueur j1, Joueur j2)
+        {
+            j1.PiocherUneCarte();
+            j2.PiocherUneCarte();
+        }
+
+        static public void DeroulementDeLaPartie(Joueur j1, Joueur j2, int numPartie)
+        {
+            Jeu.PiocherUneCarte(j1, j2);
+            //Words.Template(j1,j2, numPartie);
+            Jeu.ValiderLaVicotoire(j1, j2);
+        }
+
+
+
+        static public void ValiderLaVicotoire(Joueur j1, Joueur j2)
+        {
+            Console.WriteLine("\n");
+            if ((int)j1.carteEnJeu.point > (int)j2.carteEnJeu.point)
+            {
+                j1.Victoires++;
+                Console.WriteLine("\nVous avez gagne cette ronde ! Appuyez ENTER pour continuer");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else if ((int)j1.carteEnJeu.point < (int)j2.carteEnJeu.point)
+            {
+                j2.Victoires++;
+                Console.WriteLine("\nVous avez perdu cette ronde ! Appuyez ENTER pour continuer");
+                Console.ReadLine();
+                Console.Clear();
+            }
+
+            else if ((int)j1.carteEnJeu.point == (int)j2.carteEnJeu.point)
+            {
+                Console.WriteLine("\nBATAILLE !!");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
     }
 }
